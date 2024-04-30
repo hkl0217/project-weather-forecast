2aa4b21e8af5eab009b6af4c4f1207443b25d035
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-04-30 13:17:26 -0400

    Update index.js
    testing

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 function handleSearhSubmit(event){
     event.preventDefault();
     let cityInput = document.querySelector("#city-in-search");
-    let headingSubject = document.querySelector("#heading");
-    headingSubject.innerHTML = cityInput.value;
+    let heading = document.querySelector("#heading");
+    heading.innerHTML = cityInput.value;
 }
 
 let searchFormCity = document.querySelector("#search-form");
