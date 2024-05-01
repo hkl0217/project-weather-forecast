77c9407cbe4f9f11ed6d565ac8e7b446acee8e93
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-01 07:14:09 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-function searchCity (city) {
+function searchCity(city) {
     let apiKey = "0741f8adfdcfba62f302f1e8627toa94";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 console.log(apiUrl);
 }
 
