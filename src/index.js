6d70156ef3f3a544b8fb0765a597753af5dfa478
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-01 07:12:39 -0400

    Updated on JS
    testing out the axios integration to the code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
+function searchCity (city) {
+    let apiKey = "0741f8adfdcfba62f302f1e8627toa94";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`
+console.log(apiUrl);
+}
+
 function handleSearhSubmit(event) {
     event.preventDefault();
     let cityInput = document.querySelector("#city-in-search");
     let headingElement = document.querySelector("#heading");
     headingElement.innerHTML = cityInput.value;
+    searchCity(cityInput.value);
 }
 
 let searchFormCity = document.querySelector("#search-form");
