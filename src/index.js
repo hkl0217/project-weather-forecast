f42d14792ab81797f93515e050c80de99f60f9fa
hkl0217 <167002591+hkl0217@users.noreply.github.com>
2024-05-03 08:00:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,6 +23,10 @@ function showDate(date) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     let day = days[date.getDay()];
 
+    if (minutes <10) {
+        minutes =`0${minutes}`;
+    }
+    
     return `${day}, ${hours}:${minutes}`;
 }
 
